d63049515ff4ba79ba4e42a1e97c4ea3ed90394c
borislavr <borislavr@users.noreply.github.com>
2026-03-11 08:45:32 +0000

    Applied custom patches to cross-cluster-replication 2.19.4.0

---
+++ MAINTAINERS.md
@@ -0,0 +1,13 @@
+# OpenSearch Cross Cluster Replication Maintainers
+
+## Maintainers
+| Maintainer | GitHub ID | Affiliation |
+| --------------- | --------- | ----------- |
+| Rushi Agrawal | [rushiagr](https://github.com/rushiagr) | Amazon |
+| Gopala Krishna A | [krishna-ggk](https://github.com/krishna-ggk) | Amazon |
+| Ankit | [ankitkala](https://github.com/ankitkala) | Amazon |
+| Naveen | [naveenpajjuri](https://github.com/naveenpajjuri) | Amazon |
+| Sai | [saikaranam-amazon](https://github.com/saikaranam-amazon) | Amazon |
+| Bhanu Tadepalli | [tbhanu-amzn](https://github.com/tbhanu-amzn) | Amazon |
+| Sooraj Sinha | [soosinha](https://github.com/soosinha) | Amazon |
+| Gaurav Bafna | [gbbafna](https://github.com/gbbafna) | Amazon |
---
+++ src/main/kotlin/org/opensearch/replication/rest/StopIndexReplicationHandler.kt
@@ -0,0 +1,45 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.replication.rest
+
+import org.opensearch.replication.action.stop.StopIndexReplicationAction
+import org.opensearch.replication.action.stop.StopIndexReplicationRequest
+import org.opensearch.client.node.NodeClient
+import org.opensearch.rest.BaseRestHandler
+import org.opensearch.rest.RestChannel
+import org.opensearch.rest.RestHandler
+import org.opensearch.rest.RestRequest
+import org.opensearch.rest.action.RestToXContentListener
+import java.io.IOException
+
+class StopIndexReplicationHandler : BaseRestHandler() {
+
+    override fun routes(): List<RestHandler.Route> {
+        return listOf(RestHandler.Route(RestRequest.Method.POST, "/_plugins/_replication/{index}/_stop"))
+    }
+
+    override fun getName(): String {
+        return "plugins_index_stop_replicate_action"
+    }
+
+    @Throws(IOException::class)
+    override fun prepareRequest(request: RestRequest, client: NodeClient): RestChannelConsumer {
+        request.contentOrSourceParamParser().use { parser ->
+            val followIndex = request.param("index")
+            val stopReplicationRequest = StopIndexReplicationRequest.fromXContent(parser, followIndex)
+            return RestChannelConsumer { channel: RestChannel? ->
+                client.admin().cluster()
+                        .execute(StopIndexReplicationAction.INSTANCE, stopReplicationRequest, RestToXContentListener(channel))
+            }
+        }
+    }
+}
